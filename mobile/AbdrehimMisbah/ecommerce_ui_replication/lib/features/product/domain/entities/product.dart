487f2085aa191bbf285627bc7a55234826ee4494
Depthabdre <abdellaabdre@gmail.com>
2025-07-23 16:15:35 +0300

    remove additional method from entity

--- mobile/AbdrehimMisbah/ecommerce_ui_replication/lib/features/product/domain/entities/product.dart
+++ mobile/AbdrehimMisbah/ecommerce_ui_replication/lib/features/product/domain/entities/product.dart
@@ -17,23 +17,4 @@ class Product extends Equatable {
 
   @override
   List<Object> get props => [id];
-
-  // Factory constructor to create a Product instance from a JSON map.
-  // This is commonly used when parsing API response data.
-  factory Product.fromJson(Map<String, dynamic> json) => Product(
-    id: json['id'],
-    imageUrl: json['imageUrl'],
-    name: json['name'],
-    price: json['price'],
-    description: json['description'],
-  );
-  // Converts a Product instance into a JSON map.
-  // This is useful when sending data to an API or saving it locally.
-  Map<String, dynamic> toJson() => {
-    'id': id,
-    'imageUrl': imageUrl,
-    'name': name,
-    'price': price,
-    'description': description,
-  };
 }
