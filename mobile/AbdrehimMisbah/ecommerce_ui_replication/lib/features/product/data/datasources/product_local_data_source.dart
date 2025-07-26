06604a69d3b3e00d0a24fe7b1164f087f1b5cfb0
Depthabdre <abdellaabdre@gmail.com>
2025-07-26 19:18:04 +0300

    implement ProductLocalDataSourceImpl with caching & retrieval logic + unit tests

--- mobile/AbdrehimMisbah/ecommerce_ui_replication/lib/features/product/data/datasources/product_local_data_source.dart
+++ mobile/AbdrehimMisbah/ecommerce_ui_replication/lib/features/product/data/datasources/product_local_data_source.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../core/error/exception.dart';
 import '../models/product_model.dart';
 
 abstract class ProductLocalDataSource {
@@ -19,4 +24,106 @@ abstract class ProductLocalDataSource {
 
   /// Deletes a cached product by ID.
   Future<void> deleteProduct(int id);
+}
+
+// ignore: constant_identifier_names
+const CACHED_PRODUCT_LIST = 'CACHED_PRODUCT_LIST';
+
+// ✅ IMPLEMENTATION
+class ProductLocalDataSourceImpl implements ProductLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  ProductLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<List<ProductModel>> getLastProductList() {
+    final jsonString = sharedPreferences.getString(CACHED_PRODUCT_LIST);
+
+    if (jsonString != null) {
+      final List<dynamic> decodedJson = json.decode(jsonString);
+      final List<ProductModel> productList = decodedJson
+          .map((item) => ProductModel.fromJson(item as Map<String, dynamic>))
+          .toList();
+      return Future.value(productList);
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<ProductModel> getProductById(int id) {
+    final jsonString = sharedPreferences.getString('CACHED_PRODUCT_LIST');
+
+    if (jsonString != null) {
+      final List<dynamic> decodedList = json.decode(jsonString);
+
+      try {
+        final productMap = decodedList.firstWhere(
+          (item) => item['id'] == id,
+          orElse: () => throw CacheException(),
+        );
+
+        return Future.value(ProductModel.fromJson(productMap));
+      } catch (e) {
+        throw CacheException();
+      }
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<bool> cacheProductList(List<ProductModel> products) {
+    final jsonString = json.encode(products.map((p) => p.toJson()).toList());
+    return sharedPreferences.setString('CACHED_PRODUCT_LIST', jsonString);
+  }
+
+  @override
+  Future<void> cacheProduct(ProductModel product) async {
+    final jsonString = sharedPreferences.getString('CACHED_PRODUCT_LIST');
+    List<ProductModel> currentList = [];
+
+    if (jsonString != null) {
+      final List<dynamic> jsonList = json.decode(jsonString);
+      currentList = jsonList
+          .map((item) => ProductModel.fromJson(item))
+          .toList();
+    }
+
+    // Remove any product with same ID (to avoid duplicates)
+    currentList.removeWhere((p) => p.id == product.id);
+
+    // Add the new product
+    currentList.add(product);
+
+    final updatedJson = json.encode(
+      currentList.map((p) => p.toJson()).toList(),
+    );
+
+    await sharedPreferences.setString('CACHED_PRODUCT_LIST', updatedJson);
+  }
+
+  @override
+  Future<void> deleteProduct(int id) async {
+    final jsonString = sharedPreferences.getString('CACHED_PRODUCT_LIST');
+
+    if (jsonString != null) {
+      final List<dynamic> jsonList = json.decode(jsonString);
+      final List<ProductModel> currentList = jsonList
+          .map((item) => ProductModel.fromJson(item))
+          .toList();
+
+      final updatedList = currentList
+          .where((product) => product.id != id)
+          .toList();
+
+      final updatedJson = json.encode(
+        updatedList.map((product) => product.toJson()).toList(),
+      );
+
+      await sharedPreferences.setString('CACHED_PRODUCT_LIST', updatedJson);
+    } else {
+      throw CacheException();
+    }
+  }
 }
